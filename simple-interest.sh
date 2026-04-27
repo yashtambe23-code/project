7544cc2c83b8a6d8b5e6727f6dd07bc1ce1802a4
yashtambe23-code <yash.tambe23@vit.edu>
2026-04-27 23:46:26 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,14 @@
-#!/bin/bash echo "Enter Principal:" read principal echo "Enter Rate of Interest:" read rate echo "Enter Time Period:" read time simple_interest=$((principal * rate * time / 100)) echo "Simple Interest = $simple_interest"
+#!/bin/bash
+
+echo "Enter Principal:"
+read principal
+
+echo "Enter Rate of Interest:"
+read rate
+
+echo "Enter Time Period:"
+read time
+
+simple_interest=$((principal * rate * time / 100))
+
+echo "Simple Interest = $simple_interest"
